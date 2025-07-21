f98b01332c076b3eaf9733e76ae37ac68b2e6b01
mozbeel <matthiasleeb4@gmail.com>
2025-07-21 11:21:31 +0200

    Made it also adaptable for any desktop platfomr

--- build.zig
+++ build.zig
@@ -5,18 +5,25 @@ pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    if (target.query.os_tag == .emscripten) {
+        try buildWeb(b, target, optimize);
+    } else {
+        try buildBin(b, target, optimize);
+    }
+}
+
+fn buildWeb(b: *std.Build, target: std.Build.ResolvedTarget, optimize: std.builtin.OptimizeMode) !void {
     const activateEmsdk = zemscripten.activateEmsdkStep(b);
     b.default_step.dependOn(activateEmsdk);
 
     const wasm = b.addStaticLibrary(.{
         .name = "MyGame",
-        .root_source_file = b.path("src/main.zig"),
+        .root_source_file = b.path("src/main-web.zig"),
         .target = target,
         .optimize = optimize,
         .link_libc = true,
     });
 
-    
     const zemscripten_dep = b.dependency("zemscripten", .{});
     wasm.root_module.addImport("zemscripten", zemscripten_dep.module("root"));
 
@@ -64,3 +71,23 @@ pub fn build(b: *std.Build) !void {
     b.getInstallStep().dependOn(emcc_step);
 }
 
+fn buildBin(b: *std.Build, target: std.Build.ResolvedTarget, optimize: std.builtin.OptimizeMode) !void {
+    const exe = b.addExecutable(.{
+        .name = "MyGame",
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const sdl3 = b.dependency("sdl3", .{
+        .target = target,
+        .optimize = optimize,
+    });
+    exe.root_module.addImport("sdl3", sdl3.module("sdl3"));
+
+    exe.linkLibC();
+
+    b.installArtifact(exe);
+
+    b.getInstallStep().dependOn(&exe.step);
+}
--- src/main.zig
+++ src/main.zig
@@ -1,61 +1,28 @@
 const std = @import("std");
-const zemscripten = @import("zemscripten");
-pub const panic = zemscripten.panic;
-
-pub const std_options = std.Options{
-    .logFn = zemscripten.log,
-};
-
-
 const sdl3 = @import("sdl3");
-
 
 const SCREEN_WIDTH = 1280;
 const SCREEN_HEIGHT = 720;
 
-var should_quit: bool = false;
+pub fn main() !void {
+    defer sdl3.shutdown();
 
-fn tick() callconv(.c) void {
-    if (should_quit) {
-        zemscripten.cancelMainLoop();
-    }
-
-    while (sdl3.events.available()) {
-        switch (sdl3.events.waitAndPop() catch { should_quit = true; return; }) {
-            .quit => {
-                std.log.info("Quit event received.", .{});
-                should_quit = true;
-                break; // Exit the while loop after a quit event
-            },
-            .terminating => {
-                std.log.info("Terminating event received.", .{});
-                should_quit = true;
-                break; // Exit the while loop after a terminating event
-            },
-            else => {
-                // Handle other events here if needed (mouse, keyboard, etc.)
-                // std.log.info("Unhandled event: {any}", .{event});
-            },
-        }
-    }
-
-}
-
-export fn main() c_int {
-    std.log.info("Hello World", .{});
-    
     const init_flags = sdl3.InitFlags{ .video = true };
-    sdl3.init(init_flags) catch return 1;
+    try sdl3.init(init_flags);
     defer sdl3.quit(init_flags);
 
-    const window = sdl3.video.Window.init("Hello SDL3", SCREEN_WIDTH, SCREEN_HEIGHT, .{}) catch return 1;
+    const window = try sdl3.video.Window.init("Hello SDL3", SCREEN_WIDTH, SCREEN_HEIGHT, .{});
     defer window.deinit();
 
-    const surface = window.getSurface() catch return 1;
-    surface.fillRect(null, surface.mapRgb(128, 30, 255)) catch return 1;
-    window.updateSurface() catch return 1;
+    const surface = try window.getSurface();
+    try surface.fillRect(null, surface.mapRgb(128, 30, 255));
+    try window.updateSurface();
 
-    zemscripten.setMainLoop(tick, 0, true);
-
-    return 0;
+    while (true) {
+        switch (try sdl3.events.waitAndPop()) {
+            .quit => break,
+            .terminating => break,
+            else => {},
+        }
+    }
 }
