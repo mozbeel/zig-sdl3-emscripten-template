882123f48ccf3ed84484dffab3f77650edcacd23
mozbeel <matthiasleeb4@gmail.com>
2025-07-25 16:25:04 +0200

    Template

--- android/src/ZigSDLActivity.java
+++ android/src/ZigSDLActivity.java
@@ -6,4 +6,7 @@ import org.libsdl.app.SDLActivity;
  * A sample wrapper class that just calls SDLActivity
  */
 public class ZigSDLActivity extends SDLActivity {
+    protected String[] getLibraries() {
+        return new String[] { "SDL3", "main" };
+    }
 }
--- src/main.zig
+++ src/main.zig
@@ -2,10 +2,6 @@ const builtin = @import("builtin");
 const entry = @import("entry.zig");
 const android = @import("android");
 
-const c = @cImport({
-    @cInclude("SDL3/SDL_main.h");
-});
-
 pub const std_options: std.Options = if (builtin.abi.isAndroid())
     .{
         .logFn = android.logFn,
@@ -14,7 +10,7 @@ else
     .{};
 const std = @import("std");
 
-pub export fn SDL_main(argc : c_int, argv: [*c][*c]u8) callconv(.c) c_int {
+pub export fn main(argc: c_int, argv: [*]*?*const u8) callconv(.c) c_int {
     _ = argc;
     _ = argv;
     std.log.info("Hello World!", .{});
