fea766fa763dbe32a1c8bf2e236e1719e1f7f003
mozbeel <matthiasleeb4@gmail.com>
2025-07-29 19:22:37 +0200

    iOS

--- build.zig
+++ build.zig
@@ -40,12 +40,9 @@ fn buildBin(b: *std.Build, target: std.Build.ResolvedTarget, optimize: std.built
     const sdl_lib = sdl3.artifact("SDL3");
 
     if (target.result.os.tag == .ios) {
-        sdl_lib.addFrameworkPath(.{ .cwd_relative = b.pathJoin(&.{ b.sysroot.?, "System", "Library", "Frameworks" }) });
         exe.addFrameworkPath(.{ .cwd_relative = b.pathJoin(&.{ b.sysroot.?, "System", "Library", "Frameworks" }) });
-
         exe.addIncludePath(.{ .cwd_relative = b.pathJoin(&.{ b.sysroot.?, "usr", "include" }) });
-        sdl_lib.addIncludePath(.{ .cwd_relative = b.pathJoin(&.{ b.sysroot.?, "usr", "include" }) });
-
+        exe.addLibraryPath(b.pathFromRoot(b.pathJoin(&.{ b.sysroot.?, "usr", "lib" })));
     }
 
     exe.linkLibrary(sdl_lib);
