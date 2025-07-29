242ae0f1eaaed17fe03c8bd3c3ca95f7de00cd3d
mozbeel <matthiasleeb4@gmail.com>
2025-07-29 20:54:24 +0200

    putting main() for iOS back

--- src/main.zig
+++ src/main.zig
@@ -2,3 +2,13 @@ const builtin = @import("builtin");
 const entry = @import("entry.zig");
 const std = @import("std");
 
+pub fn main() !void {
+    try entry.init();
+    defer entry.destroy();
+
+    while (entry.running) {
+        entry.event();
+        entry.iterate();
+    }
+}
+
