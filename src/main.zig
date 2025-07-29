fce269a16c70600b4f4004130cf1894b7d2079e8
mozbeel <matthiasleeb4@gmail.com>
2025-07-29 20:51:45 +0200

    Testing iOS

--- src/main.zig
+++ src/main.zig
@@ -2,14 +2,3 @@ const builtin = @import("builtin");
 const entry = @import("entry.zig");
 const std = @import("std");
 
-pub fn main() !void {
-    try entry.init();
-    defer entry.destroy();
-
-    while (entry.running) {
-        entry.event();
-        entry.iterate();
-    }
-}
-
-
